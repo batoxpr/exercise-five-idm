fbf53028c7977f1a486c00b9b0cceb9956cf5a09
Bato Batbileg <batoxpr@10-18-230-179.dynapool.wireless.nyu.edu>
2023-11-07 09:47:37 -0500

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+const firebase = require("firebase/app")
+const port = 4000
+
+//project config unique number
+const firebaseConfig = {
+    apiKey: "AIzaSyBa7dkLgQp0wjrWXEAncfAIByE0zc3j_34",
+    authDomain: "exercise-five-idm.firebaseapp.com",
+    projectId: "exercise-five-idm",
+    storageBucket: "exercise-five-idm.appspot.com",
+    messagingSenderId: "639521384602",
+    appId: "1:639521384602:web:490988c98b151342bb5723"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+const indexRoute = require('./routes/index');
+// const createPostRoute = require('./routes/createPost');
+// const singlePostRoute = require('./routes/singlePost');
+
+app.use("/", indexRoute);
+  
+app.listen(port, () => {
+    console.log(`Exercise five on port ${port}`)
+  });
