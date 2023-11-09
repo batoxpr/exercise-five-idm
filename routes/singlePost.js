d45e091973adfa16343fbf5703a90cd5ab06114f
Bato Batbileg <batoxpr@10-18-155-74.dynapool.wireless.nyu.edu>
2023-11-09 09:45:19 -0500

    nov9

---
+++ routes/singlePost.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const firestore = require("firebase/firestore");
+const db = firestore.getFirestore();
+
+router.get("/:postId", (req, res) => {
+    const postId = req.params.postId;
+    const postsQuery= firestore.getDoc(firestore.doc(db, "posts", postId));
+
+    postsQuery
+       .then((response) => {
+        res.send(response.data());
+        })
+       .catch((error) => {
+        console.log(error);
+        return res.send(error);
+       });
+    });
+
+module.exports = router;
